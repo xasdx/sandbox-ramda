e29bb9e1299cfed73aaffd34abc616845a644da4
_asdx_ <xasdx@tuta.io>
2018-03-29 12:30:21 +0000

    Curry functions with R.curry and R.curryN

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let { expect } = require("chai")
 let R = require("ramda")
+let _ = R.__
 
 Object.values({
   
@@ -11,8 +12,28 @@ Object.values({
     expect(array).to.deep.equal([0, 1, 2])
     expect(otherArray).to.deep.equal([0, 3, 2])
   },
-  "replacesForLoopWithARange": () => {
+  "generatesRanges": () => {
     
     expect(R.range(2, 5).reverse().map(n => n + 1)).to.deep.equal([5, 4, 3])
+  },
+  "curriesFunctions": () => {
+    
+    let add = (a, b) => a + b
+    let subtract = (a, b) => a - b
+    let sum = (...args) => R.sum(args)
+    
+    let f = R.curry(add)(5)
+    expect(f(6)).to.equal(11)
+    
+    let g = R.curry(add)
+    expect(g(5, 7)).to.equal(12)
+    
+    let h = R.curry(subtract)(_, 10)
+    expect(h(5)).to.equal(-5)
+    
+    expect(sum(1, 2, 3)).to.equal(6)
+    
+    let i = R.curryN(2, sum)
+    expect(i(4, 5)).to.equal(9)
   }
 }).forEach(f => f())
